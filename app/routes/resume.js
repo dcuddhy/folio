f80d7f19735227d3a3a71f2cbe4135d45f698d2a
dcuddhy <dcuddhy@gmail.com>
2015-01-27 18:18:06 -0700

    dumby thumbnails in place.  menu pullout blue

--- app/routes/resume.js
+++ app/routes/resume.js
@@ -8,30 +8,36 @@ export default Ember.Route.extend({
       desc: 'Ruby on Rails, Twitter Bootstrap, Custom CSS, Permissions <br>' +
             'APIs - Facebook, Tumblr, Flickr, Instagram, 500px, 4,100 followers <br>'},
 
-    { name: 'Drew Cuddhy Photography',
+   { thumb: '<img src= "http://amcuae.com/wp-content/uploads/2014/01/734465_602209193174852_791866153_n.jpg" >',
+      name: 'Drew Cuddhy Photography',
       desc: 'Custom CSS, JavaScript, Wordpress, Storefront <br>' +
             'Eye-Fi Inegration through Flickr for live blogging events.<br>'},
 
-    { name: 'This Website',
+   { thumb: '<img src= "http://amcuae.com/wp-content/uploads/2014/01/734465_602209193174852_791866153_n.jpg" >',
+      name: 'This Website',
       desc: 'JavaScript, Ember, Handlebars, Custom CSS <br>' +
             'Web development portfolio aka, space for me to <em>strut.</em>'},
 
-    { name: 'Rantly',
+   { thumb: '<img src= "http://www.mikeledda.com/img/thumb-andela-website.jpg" >',
+      name: 'Rantly',
       desc: 'Ruby on Rails, Javascript, Mustache, Custom CSS <br>' +
             'Like Twitter, but better.'},
 
-    { name: 'gCamp',
+   { thumb:'<img src= "http://www.mikeledda.com/img/thumb-andela-website.jpg" >',
+      name: 'gCamp',
       desc: 'Ruby on Rails, Twitter Bootsrap, Extensive Associations & Permissions <br>' +
-            'A Rails app that has some of the same functionality as Basecamp.'},
+            'A Rails app inspired by as Basecamp.'},
 
-    { name: 'Snipits Template',
+   { thumb: '<img src= "http://www.mikeledda.com/img/pic-edison-displays-home.jpg" >',
+      name: 'Snipits Template',
       desc: 'CSS, SASS, Mustache, Awesome Bootsrap <br>' +
             'A Jekyll template.'},
 
-    { name: 'Music Player',
+   { thumb: '<img src= "http://amcuae.com/wp-content/uploads/2014/01/734465_602209193174852_791866153_n.jpg" >',
+      name: 'Music Player',
       desc: 'Mustache, AJAX, HTML5 Audio & CSS. <br>' +
             'A simple music player.'}
-    ]
+    ];
 }
 
 });

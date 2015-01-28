c6c2d595696deff367d5815007d54a068aa5c950
dcuddhy <dcuddhy@gmail.com>
2015-01-27 22:05:16 -0700

    added dumby text to trainings and fixed menu slide transition to be smoother

--- app/controllers/resume.js
+++ app/controllers/resume.js
@@ -3,8 +3,18 @@ import Ember from 'ember';
 export default Ember.ArrayController.extend({
 
   trainings: [
-    {name: "Photows", desc: "dudes" },
-    {name: "Filler", desc: "holler"}
+    { name: "gSchool",
+      desc: "Full-Stack Immersive <br> Ruby on Rails, Ember",
+      date: "April 2015"},
+    { name: "xSmallBiz",
+      desc: "Work Study <br> Twitter API integration",
+      date: "March 2015"},
+    { name: "yStartupCO",
+      desc: "Work Study <br> Rebuilt responsive site",
+      date: "March 2015"},
+    { name: "zNeighborTech",
+      desc: "Work Study <br> Created new database structure",
+      date: "February 2015"}
   ]
 
 });

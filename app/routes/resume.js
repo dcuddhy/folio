98c275c3826f55f7f645457418c52373d9a1bfc7
dcuddhy <dcuddhy@gmail.com>
2015-02-10 14:46:52 -0700

    webfonts in and responsive header.  commit more often

--- app/routes/resume.js
+++ app/routes/resume.js
@@ -8,7 +8,7 @@ export default Ember.Route.extend({
       desc: 'Ruby on Rails, Twitter Bootstrap, Custom CSS, Permissions <br>' +
             'APIs - Facebook, Tumblr, Flickr, Instagram, 500px, 4,100 followers <br>'},
 
-   { thumb: '<img src= "http://amcuae.com/wp-content/uploads/2014/01/734465_602209193174852_791866153_n.jpg" >',
+   { thumb: '<img src= "assets/screenshots/dcp-sm-bx.jpg" >',
       name: 'Drew Cuddhy Photography',
       desc: 'Custom CSS, JavaScript, Wordpress, Storefront <br>' +
             'Eye-Fi Inegration through Flickr for live blogging events.<br>'},
@@ -23,7 +23,7 @@ export default Ember.Route.extend({
       desc: 'Ruby on Rails, Javascript, Mustache, Custom CSS <br>' +
             'Like Twitter, but better.'},
 
-   { thumb:'<img src= "http://www.mikeledda.com/img/thumb-andela-website.jpg" >',
+   { thumb:'<img src= "assets/screenshots/gcamp-sm-bx.jpg" >',
       name: 'gCamp',
       desc: 'Ruby on Rails, Twitter Bootsrap, Extensive Associations & Permissions <br>' +
             'A Rails app inspired by as Basecamp.'},
@@ -33,12 +33,12 @@ export default Ember.Route.extend({
       desc: 'CSS, SASS, Mustache, Awesome Bootsrap <br>' +
             'A Jekyll template.'},
 
-   { thumb: '<img src= "http://amcuae.com/wp-content/uploads/2014/01/734465_602209193174852_791866153_n.jpg" >',
+   { thumb: '<img src= "assets/screenshots/mde-sm-bx.jpg" >',
       name: 'Markdown Editor',
       desc: 'Mustache, AJAX, HTML5 Audio & CSS. <br>' +
             'A simple music player. A simple music player.'},
 
-   { thumb: '<img src= "http://amcuae.com/wp-content/uploads/2014/01/734465_602209193174852_791866153_n.jpg" >',
+   { thumb: '<img src= "assets/screenshots/listen-sm-bx.jpg" >',
       name: 'Music Player',
       desc: 'Mustache, AJAX, HTML5 Audio & CSS. <br>' +
             'A simple music player.'},

9a3779bb52b39123ca5c1ecb96f965e973a17031
dcuddhy <dcuddhy@gmail.com>
2015-02-25 14:22:46 -0700

    filled out projects, stripped down resume and added actual content including thumbs

--- app/routes/projects.js
+++ app/routes/projects.js
@@ -4,34 +4,50 @@ export default Ember.Route.extend({
 
   model: function() {
     return [
-      { thumb: '<img src= "http://www.mikeledda.com/img/thumb-andela-website.jpg" >',
-        name: 'The Beard Club',
-        desc: 'Ruby on Rails, Twitter Bootstrap, Custom CSS, Permissions <br>' +
-                'APIs - Facebook, Tumblr, Flickr, Instagram, 500px, 4,100 followers <br>'},
+  {  thumb: '<img src= "http://www.mikeledda.com/img/thumb-andela-website.jpg" >',
+      name: 'The Beard Club',
+      desc: 'Ruby on Rails, Bootstrap, Custom CSS, Permissions <br>' +
+            'APIs - Facebook, Tumblr, Instagram, 4,100 followers.'},
 
-      { name: 'Drew Cuddhy Photography',
-        desc: 'Custom CSS, JavaScript, Wordpress, Storefront <br>' +
-              'Eye-Fi Inegration through Flickr for live blogging events.<br>'},
+   { thumb: '<img src= "assets/screenshots/dcp-sm-bx.jpg" >',
+      name: 'Drew Cuddhy Photography',
+      desc: 'Custom CSS, JavaScript, Wordpress, Storefront <br>' +
+            'Eye-Fi Inegration for live blogging events.'},
 
-      { name: 'This Website',
-        desc: 'JavaScript, Ember, Handlebars, Custom CSS <br>' +
-              'Web development portfolio aka, space for me to <em>strut.</em>'},
+   { thumb: '<img src= "http://amcuae.com/wp-content/uploads/2014/01/734465_602209193174852_791866153_n.jpg" >',
+      name: 'This Website',
+      desc: 'JavaScript, Ember, Handlebars, Custom CSS <br>' +
+            'Web development portfolio aka, space for me to <em>strut.</em>'},
 
-      { name: 'Rantly',
-        desc: 'Ruby on Rails, Javascript, Mustache, Custom CSS <br>' +
-              'Like Twitter, but better.'},
+   { thumb: '<img src= "http://www.mikeledda.com/img/thumb-andela-website.jpg" >',
+      name: 'Rantly',
+      desc: 'Ruby on Rails, Javascript, Mustache, Custom CSS <br>' +
+            'Like Twitter, but better.'},
 
-      { name: 'gCamp',
-        desc: 'Ruby on Rails, Twitter Bootsrap, Extensive Associations & Permissions <br>' +
-              'A Rails app that has some of the same functionality as Basecamp.'},
+   { thumb:'<img src= "assets/screenshots/gcamp-sm-bx.jpg" >',
+      name: 'gCamp',
+      desc: 'Ruby on Rails, Bootsrap, Associations & Permissions <br>' +
+            'A Rails app inspired by as Basecamp.'},
 
-      { name: 'Snipits Template',
-        desc: 'CSS, SASS, Mustache, Awesome Bootsrap <br>' +
-              'A Jekyll template.'},
+   { thumb: '<img src= "http://www.mikeledda.com/img/pic-edison-displays-home.jpg" >',
+      name: 'Snipits Template',
+      desc: 'CSS, SASS, Mustache, Awesome Bootsrap <br>' +
+            'A Jekyll template.'},
 
-      { name: 'Music Player',
-        desc: 'Mustache, AJAX, HTML5 Audio & CSS. <br>' +
-              'A simple music player.'}
+   { thumb: '<img src= "assets/screenshots/mde-sm-bx.jpg" >',
+      name: 'Markdown Editor',
+      desc: 'Mustache, AJAX, HTML5 Audio & CSS. <br>' +
+            'A simple music player. A simple music player.'},
+
+   { thumb: '<img src= "assets/screenshots/listen-sm-bx.jpg" >',
+      name: 'Music Player',
+      desc: 'Mustache, AJAX, HTML5 Audio & CSS. <br>' +
+            'A simple music player.'},
+
+   { thumb: '<img src= "http://www.mikeledda.com/img/pic-edison-displays-home.jpg" >',
+      name: 'Notes',
+      desc: 'CSS, SASS, Mustache, Awesome Bootsrap <br>' +
+            'Users host their own local data in browser for unmatched security.'}
     ];
   }
 });
--- app/routes/resume.js
+++ app/routes/resume.js
@@ -3,50 +3,29 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     return [
-  {  thumb: '<img src= "http://www.mikeledda.com/img/thumb-andela-website.jpg" >',
+  {  thumb: '<img src= "assets/screenshots/tbcdc-sm-lil.jpg" >',
       name: 'The Beard Club',
-      desc: 'Ruby on Rails, Bootstrap, Custom CSS, Permissions <br>' +
-            'APIs - Facebook, Tumblr, Instagram, 4,100 followers.'},
-
-   { thumb: '<img src= "assets/screenshots/dcp-sm-bx.jpg" >',
-      name: 'Drew Cuddhy Photography',
-      desc: 'Custom CSS, JavaScript, Wordpress, Storefront <br>' +
-            'Eye-Fi Inegration for live blogging events.'},
-
-   { thumb: '<img src= "http://amcuae.com/wp-content/uploads/2014/01/734465_602209193174852_791866153_n.jpg" >',
-      name: 'This Website',
-      desc: 'JavaScript, Ember, Handlebars, Custom CSS <br>' +
-            'Web development portfolio aka, space for me to <em>strut.</em>'},
-
-   { thumb: '<img src= "http://www.mikeledda.com/img/thumb-andela-website.jpg" >',
-      name: 'Rantly',
-      desc: 'Ruby on Rails, Javascript, Mustache, Custom CSS <br>' +
-            'Like Twitter, but better.'},
+      desc: 'A curated collection of beard-centric content.<br>' +
+            'Ruby on Rails, Sass, SVGs, multiple API calls,<br>' +
+            'data parsing, responsive design, 4,100+ Followers.'},
 
    { thumb:'<img src= "assets/screenshots/gcamp-sm-bx.jpg" >',
       name: 'gCamp',
-      desc: 'Ruby on Rails, Bootsrap, Associations & Permissions <br>' +
-            'A Rails app inspired by as Basecamp.'},
+      desc: 'Ruby on Rails, Twitter Bootstrap, administrators,<br>' +
+            'multiple user projects, tasks with comments and due<br>' +
+            'dates, dual application layouts.<br>'},
 
-   { thumb: '<img src= "http://www.mikeledda.com/img/pic-edison-displays-home.jpg" >',
-      name: 'Snipits Template',
-      desc: 'CSS, SASS, Mustache, Awesome Bootsrap <br>' +
-            'A Jekyll template.'},
+   { thumb: '<img src= "assets/screenshots/dcp-sm-bx.jpg" >',
+      name: 'Drew Cuddhy Photography',
+      desc: 'A pre-gSchool photo portfolio & client hub. <br>' +
+            'JavaScript image gallery, Wordpress, custom CSS <br>' +
+            'shopping cart, live photo blogging integration. <br>'},
 
-   { thumb: '<img src= "assets/screenshots/mde-sm-bx.jpg" >',
-      name: 'Markdown Editor',
-      desc: 'Mustache, AJAX, HTML5 Audio & CSS. <br>' +
-            'A simple music player. A simple music player.'},
-
-   { thumb: '<img src= "assets/screenshots/listen-sm-bx.jpg" >',
-      name: 'Music Player',
-      desc: 'Mustache, AJAX, HTML5 Audio & CSS. <br>' +
-            'A simple music player.'},
-
-   { thumb: '<img src= "http://www.mikeledda.com/img/pic-edison-displays-home.jpg" >',
-      name: 'Local Notes',
-      desc: 'CSS, SASS, Mustache, Awesome Bootsrap <br>' +
-            'Users host their own local data in browser for unmatched security.'}
+   { thumb: '<img src= "assets/screenshots/notes-sm-bxr.jpg" >',
+      name: 'Notes',
+      desc: 'A Stickies inspired note taking app. <br>' +
+            'Ruby on Rails back end, Ember CLI front end, Font <br>' +
+            'Awesome icons, flash messages.'}
     ];
 }
 

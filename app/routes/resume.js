3df1a208f4c1648618c23367274ab1a1018e6534
dcuddhy <dcuddhy@gmail.com>
2015-03-05 23:03:04 -0700

    added new thumbnails, bg image, increased title fonts, deleted drop shadows, changed color of wrap

--- app/routes/resume.js
+++ app/routes/resume.js
@@ -9,13 +9,13 @@ export default Ember.Route.extend({
             'Ruby on Rails, Sass, SVGs, multiple API calls,<br>' +
             'data parsing, responsive design, 4,100+ Followers.'},
 
-   { thumb:'<img src= "assets/screenshots/gcamp-sm-bx.jpg" >',
+   { thumb:'<img src= "assets/screenshots/gcamp-sm-bxr.jpg" >',
       name: 'gCamp',
       desc: 'Ruby on Rails, Twitter Bootstrap, administrators,<br>' +
             'multiple user projects, tasks with comments and due<br>' +
             'dates, dual application layouts.<br>'},
 
-   { thumb: '<img src= "assets/screenshots/dcp-sm-bx.jpg" >',
+   { thumb: '<img src= "assets/screenshots/dcp-sm-bxr.jpg" >',
       name: 'Drew Cuddhy Photography',
       desc: 'A pre-gSchool photo portfolio & client hub. <br>' +
             'JavaScript image gallery, Wordpress, custom CSS <br>' +

76870c8d09b47900006f890c5e1441b2e3e75b12
dcuddhy <dcuddhy@gmail.com>
2015-03-22 13:32:49 -0600

    added favicons to resume page folio pieces

--- app/routes/resume.js
+++ app/routes/resume.js
@@ -4,25 +4,25 @@ export default Ember.Route.extend({
   model: function() {
     return [
   {  thumb: '<a href= "https://tbcdc.herokuapp.com" target="_blank"> <img src= "assets/screenshots/tbcdc-sm-lil.jpg" > </a>',
-      name: 'The Beard Club',
+      name: '<img src= "assets/favicons/tbcdc-favi.svg"> The Beard Club',
       desc: 'A curated collection of beard-centric content.<br>' +
             'Ruby on Rails, Sass, SVGs, multiple API calls,<br>' +
-            'data parsing, responsive design, 4,100+ Followers.'},
+            'data parsing, responsive design, 4,100+ followers.'},
 
    { thumb:'<a href= "https://mysterious-citadel-5725.herokuapp.com/" target="_blank"> <img src= "assets/screenshots/gcamp-sm-lil.jpg" > </a>',
-      name: 'gCamp',
+      name: '<img src= "assets/favicons/gcamp-favi.svg"> gCamp',
       desc: 'Ruby on Rails, Twitter Bootstrap, administrators,<br>' +
             'multiple user projects, tasks with comments and due<br>' +
             'dates, dual application layouts.<br>'},
 
    { thumb: '<a href= "http://www.drewcuddhy.com" target="_blank"> <img src= "assets/screenshots/dcp-sm-lil.jpg" > </a>',
-      name: 'Drew Cuddhy Photography',
+      name: '<img src= "assets/favicons/dcp-favi.svg"> Drew Cuddhy Photography',
       desc: 'A pre-gSchool photo portfolio & client hub. <br>' +
             'JavaScript image gallery, Wordpress, custom CSS <br>' +
             'shopping cart, live photo blogging integration. <br>'},
 
    { thumb: '<a href= "https://warm-island-1114.herokuapp.com/notes" target="_blank"> <img src= "assets/screenshots/notes-sm-bxr.jpg" > </a>',
-      name: 'Notes',
+      name: '<img src= "assets/favicons/notes-favi.svg"> Notes',
       desc: 'A Stickies inspired note taking app. <br>' +
             'Ruby on Rails backend, Ember CLI frontend, Font <br>' +
             'Awesome icons, flash messages.'}
